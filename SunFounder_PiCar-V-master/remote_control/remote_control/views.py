68520da83f06b6c0f75adcba90663550f933e4ed
WitseMeeussen <g6fs2v3c5b74c4@gmail.com>
2021-12-08 15:31:30 +0100

    captureing V0

--- SunFounder_PiCar-V-master/remote_control/remote_control/views.py
+++ SunFounder_PiCar-V-master/remote_control/remote_control/views.py
@@ -15,6 +15,29 @@ from .driver import camera, stream
 from picar import back_wheels, front_wheels
 from django.http import HttpResponse
 import picar
+import cv2
+import os
+from datetime import datetime
+
+
+folder = '/root/Data/' + datetime.now()
+
+def captureData():
+	if(ControlerData['drive']==0): 
+		return
+	if not os.path.isdir(folder): 
+		os.mkdir(folder)
+	camera = cv2.VideoCapture(-1)
+	camera.set(3, 640)
+	camera.set(4, 480)
+
+
+	
+	_, image = camera.read()        
+	cv2.imwrite("%s_%03d_%03d.png" % ( datetime.hour+datetime.minute, ControlerData['speed'], ControlerData['direction']), image)
+        
+    
+
 
 picar.setup()
 db_file = "/home/pi/SunFounder_PiCar-V/remote_control/remote_control/driver/config"
@@ -28,30 +51,46 @@ fw.ready()
 SPEED = 60
 bw_status = 0
 
+ControlerData = {
+		'speed':1,
+		'drive':0,
+		'direction':0
+}
+
 print(stream.start())
 
 def home(request):
 	return render_to_response("base.html")
 
 def run(request):
+	### this contains al the controller input
+	ControlerData = {
+		'speed':0,
+		'drive':0,
+		'direction':0
+	}
 	global SPEED, bw_status
 	debug = ''
 	if 'action' in request.GET:
 		action = request.GET['action']
 		# ============== Back wheels =============
 		if action == 'bwready':
+			ControlerData['drive'] = 0
 			bw.ready()
 			bw_status = 0
 		elif action == 'forward':
+			ControlerData['drive'] = 1
 			bw.speed = SPEED
 			bw.forward()
 			bw_status = 1
 			debug = "speed =", SPEED
 		elif action == 'backward':
+			ControlerData['drive'] = 0
 			bw.speed = SPEED
 			bw.backward()
 			bw_status = -1
 		elif action == 'stop':
+			ControlerData['drive'] = 0
 			bw.stop()
 			bw_status = 0
 
@@ -59,10 +98,13 @@ def run(request):
 		elif action == 'fwready':
 			fw.ready()
 		elif action == 'fwleft':
+			ControlerData['direction'] = -1
 			fw.turn_left()
 		elif action == 'fwright':
+			ControlerData['direction'] = 0
 			fw.turn_right()
 		elif action == 'fwstraight':
+			ControlerData['direction'] = 1
 			fw.turn_straight()
 		elif 'fwturn' in action:
 			print("turn %s" % action)
@@ -89,6 +131,7 @@ def run(request):
 		if bw_status != 0:
 			bw.speed = SPEED
 		debug = "speed =", speed
+	captureData()
 	host = stream.get_host().decode('utf-8').split(' ')[0]
 	return render_to_response("run.html", {'host': host})
 
